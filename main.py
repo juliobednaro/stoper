2df2fe083ebc23a889969ddf82372919dba43fe5
polskiprogramator <33041529+polskiprogramator@users.noreply.github.com>
2022-02-02 13:00:31 +0100

    cleaned code a bit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600,100))
+pygame.display.set_caption("Stopwatch")
+
+class Stopwatch:
+    def __init__(self, screen):
+        self.screen = screen
+        self.ms = 0
+        self.seconds = 0
+        self.minutes = 0
+        self.current_time = "0:0:0"
+        self.font = pygame.font.SysFont("comicsans", 64)
+        self.stop = False
+        self.CLOCK_STOPPED_TEXT = self.font.render("STOPPED", False, (255,255, 0))
+
+    def change_clock_state(self):
+        self.stop = not self.stop
+
+    def update_clock(self):
+        if not self.stop:
+            self.ms += 1
+            if self.ms % 60 == 0:
+                self.seconds += 1
+            if self.seconds == 60:
+                self.seconds = 0
+                self.minutes += 1
+        self.current_time = self.font.render(f"{self.minutes}:{self.seconds}:{self.ms % 60}", False, (255,255,255))
+
+    def show_clock(self):
+        self.screen.fill((0,0,0))
+        self.screen.blit(self.current_time, (236,0))
+        if self.stop:
+            self.screen.fill((255,0,0))
+            self.screen.blit(self.CLOCK_STOPPED_TEXT, ((600 - self.CLOCK_STOPPED_TEXT.get_width() )//2, 0))
+
+    def update_screen(self):
+        self.update_clock()
+        self.show_clock()
+
+
+run = True
+clock = pygame.time.Clock()
+s = Stopwatch(screen)
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                s.change_clock_state()
+    s.update_screen()
+    pygame.display.update()
